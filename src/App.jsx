66993a8536e001e006169b4280867be96511846e
Divyansh Yadav <divyanshyadav206@gmail.com>
2025-08-20 23:28:12 +0530

    Update App.jsx
    - Added explanatory comments for imports, component, and export
    - Improved spacing and readability
    - No changes to logic or functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from 'react'   // React import (needed for JSX)
+import Navbar from './components/Navbar'  // Importing Navbar component
 
+// Root App Component
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      {/* Rendering Navbar at the top */}
+      <Navbar />  
     </div>
   )
 }
 
-export default App
+export default App  // Exporting App so it can be used in index.js
